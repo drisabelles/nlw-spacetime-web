7bb4be0a807ff6d2cb908d9ad30e2bc5598ff72d
Isabelle <drsabelles@outlook.com>
2023-05-18 18:05:14 -0300

    chore: finishing the setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
-  return <h1>Hello Word</h1>;
+  return (
+    <div className="h-screen bg-zinc-900 text-zinc-50 p-6">
+      <h1 className="text-4xl font-bold">Sua cápsula do tempo dos projetos</h1>
+    </div>
+  );
 }
