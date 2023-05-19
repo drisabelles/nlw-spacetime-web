a8936fa10b849b042ced878e868d941f15c7c31f
Isabelle <drsabelles@outlook.com>
2023-05-19 19:36:01 -0300

    feat: creating components

---
+++ src/components/Copyright.tsx
@@ -0,0 +1,16 @@
+export function Copyright() {
+  return (
+    <div className="text-sm leading-relaxed text-gray-200">
+      Feito com 💜 por{" "}
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href="https://www.linkedin.com/in/drisabelles/"
+        className="underline hover:text-gray-100"
+      >
+        Isabelle Ribeiro
+      </a>
+      .
+    </div>
+  );
+}
