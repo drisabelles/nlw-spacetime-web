3aaf027e943b7c5b825d57a570349d86a38ca9c8
Isabelle <drsabelles@outlook.com>
2023-05-18 23:40:17 -0300

    feat: adding fonts configuration, visual structure for home page and empty memories field

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,27 @@
 export default function Home() {
   return (
-    <div className="h-screen bg-zinc-900 text-zinc-50 p-6">
-      <h1 className="text-4xl font-bold">Sua cápsula do tempo dos projetos</h1>
-    </div>
+    <main className="grid grid-cols-2 min-h-screen">
+      {/* left */}
+      <div className="relative px-28 py-16 flex flex-col items-start justify-between overflow-hidden border-r border-white/10">
+        {/* blur */}
+        <div className="absolute right-0 top-1/2 h-[288px] w-[526px] bg-purple-700 opacity-50 -translate-y-1/2 translate-x-1/2 rounded-full blur-full" />
+
+        {/* stripes */}
+        <div className="absolute bottom-0 right-1 top-0 w-2 bg-stripes" />
+      </div>
+
+      {/* right */}
+      <div className="flex flex-col p-16">
+        <div className="flex flex-1 items-center justify-center">
+          <p className="text-center leading-relaxed w-[360px]">
+            Você ainda não registrou nenhuma lembrança, comece a{" "}
+            <a href="" className="underline hover:text-gray-50">
+              criar agora
+            </a>
+            !
+          </p>
+        </div>
+      </div>
+    </main>
   );
 }
