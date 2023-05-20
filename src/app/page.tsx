b95a2d7b35649eb86e324871918e0a69b4a08170
Isabelle <drisabelles@outlook.com>
2023-05-20 16:28:16 -0300

    feat: using cookies for authentication and creating user profile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
+import { cookies } from "next/headers";
 import { Copyright } from "@/components/Copyright";
 import { EmptyMemories } from "@/components/EmptyMemories";
 import { Hero } from "@/components/Hero";
 import { SignIn } from "@/components/SignIn";
+import { Profile } from "@/components/Profile";
+
 export default function Home() {
+  const isAuthenticated = cookies().has("token");
+
   return (
     <main className="grid grid-cols-2 min-h-screen">
       {/* left */}
@@ -11,7 +16,7 @@ export default function Home() {
         <div className="absolute right-0 top-1/2 h-[288px] w-[526px] bg-purple-700 opacity-50 -translate-y-1/2 translate-x-1/2 rounded-full blur-full" />
         {/* stripes */}
         <div className="absolute bottom-0 right-1 top-0 w-2 bg-stripes" />
-        <SignIn />
+        {isAuthenticated ? <Profile /> : <SignIn />}
         <Hero />
         <Copyright />
       </div>
